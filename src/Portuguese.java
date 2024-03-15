f286a1fcbb3058be29a3ff4b2556b6c1b5dea927
BT_1N3_08 <sample@bths.edu>
2024-03-15 09:30:21 -0400

    working on dictionary (bandaged head emoji)

--- src/Portuguese.java
+++ src/Portuguese.java
@@ -7,14 +7,14 @@ public class Portuguese {
     }
 
     public void start() {
-        System.out.print("ESCOLHA A LINGUA/PICK YOUR LANGUAGE (pt/en): ");
+        System.out.print("ESCOLHA A LÍNGUA/PICK YOUR LANGUAGE (pt/en): ");
         lang = s.nextLine();
         while (!lang.toLowerCase().equals("pt") && !lang.toLowerCase().equals("en")) {
-            System.out.print("Isso não é uma opção, pede-lhe de escholher de novo/That's not an option, please try again: ");
+            System.out.print("Isso não é uma opção, pede-lhe de escolher de novo/That's not an option, please try again: ");
             lang = s.nextLine();
         }
         if (lang.toLowerCase().equals("pt")) {
-            System.out.println("Olá, bem-vindo/a ao ajudor gramático do português");
+            System.out.println("Olá, bem-vindo/a ao assistente gramático do português");
             lang = "p";
         } else {
             System.out.println("Hello, welcome to the portuguese grammar helper");
@@ -31,12 +31,22 @@ public class Portuguese {
                 System.out.print("Por favor tecle \"n\" ou \"v\": ");
                 str = s.nextLine();
             }
+            if (str.toLowerCase().equals("n")) {
+                System.out.println("Escolha um substantivo da liste por favor: ");
+            } else {
+                System.out.println("Escolha um verbo da liste por favor: ");
+            }
         } else {
             System.out.println("What would you like help with? (n)ouns or (v)erbs");
             String str = s.nextLine();
             while (!str.toLowerCase().equals("n") && !str.toLowerCase().equals("v")) {
                 System.out.print("Please input \"n\" or \"v\": ");
                 str = s.nextLine();
+            }
+            if (str.toLowerCase().equals("n")) {
+                System.out.println("Please pick a noun from the list: ");
+            } else {
+                System.out.println("Please pick a verb from the list: ");
             }
         }
     }
